88ab684d712c4fa761479da4310513191f1761a6
WoutDeBolster <78808866+WoutDeBolster@users.noreply.github.com>
2023-03-21 12:47:52 +0100

    Revert "tried obj reader again but cant get te binary conversion right"
    This reverts commit 73be64cbf246fa7ca8f11c957c2253540668dbbd.

--- Minigin/ObjReader.cpp
+++ Minigin/ObjReader.cpp
@@ -6,8 +6,7 @@ ObjReader::ObjReader(std::string fileLoc)
 {
 }
 
-void ObjReader::ParseObjFile(std::vector<Vertex>& vertices, std::vector<VertexNormals>& vertexNormals,
-	std::vector<Face>& faces, std::vector<std::string>& commands)
+void ObjReader::ParseObjFile(std::vector<Vertex>& vertices, std::vector<Face>& faces)
 {
 	std::ifstream objFile;
 	std::string line;
@@ -21,32 +20,20 @@ void ObjReader::ParseObjFile(std::vector<Vertex>& vertices, std::vector<VertexNo
 			{
 				GetVertex(line);
 			}
-			if (line.find("vn", 0) == 0)
-			{
-				GetVertexNormal(line);
-			}
 			if (line.find("f", 0) == 0)
 			{
 				GetFace(line);
 			}
-			if (line.find("#", 0) == 0)
-			{
-				GetCommand(line, static_cast<char>(line.size()));
-			}
 		}
 
 		vertices = m_Vertices;
-		vertexNormals = m_VertexNormals;
 		faces = m_Faces;
-		commands = m_Commands;
-
-		m_HasParsed = true;
 	}
 }
 
 void ObjReader::WriteBobj(const std::string& fileName)
 {
-	if (m_HasParsed)
+	if (m_Faces.size() != 0 && m_Vertices.size() != 0)
 	{
 		if (std::ofstream myOutput{ fileName, std::ios::binary }; myOutput.is_open())
 		{
@@ -56,23 +43,10 @@ void ObjReader::WriteBobj(const std::string& fileName)
 				myOutput.write((const char*)&vertex.x, sizeof(float) * 3);
 				myOutput << "\r\n"; // new line
 			}
-			for (VertexNormals vertexNormal : m_VertexNormals)
-			{
-				myOutput.write((const char*)"vn", sizeof(char));
-				myOutput.write((const char*)&vertexNormal.x, sizeof(float) * 3);
-				myOutput << "\r\n"; // new line
-			}
 			for (Face face : m_Faces)
 			{
 				myOutput.write((const char*)"f", sizeof(char));
 				myOutput.write((const char*)&face.x, sizeof(float) * 3);
-				myOutput << "\r\n"; // new line
-			}
-			for (std::string command : m_Commands)
-			{
-				myOutput.write((const char*)"#", sizeof(char));
-				myOutput.put(static_cast<char>(command.size()));
-				myOutput.write((const char*)&m_Commands, sizeof(command));
 				myOutput << "\r\n"; // new line
 			}
 		}
@@ -88,27 +62,11 @@ void ObjReader::GetVertex(const std::string& line)
 	m_Vertices.push_back(result);
 }
 
-void ObjReader::GetVertexNormal(const std::string& line)
-{
-	VertexNormals result{ 0, 0, 0 };
-	sscanf_s(line.c_str(), "v %f %f %f", &result.x, &result.y, &result.z);
-
-	m_VertexNormals.push_back(result);
-}
-
 void ObjReader::GetFace(const std::string& line)
 {
 	Face result{ 0, 0, 0 };
+	// https://www.ibm.com/docs/en/zos/2.3.0?topic=programs-sscanf-read-format-data
 	sscanf_s(line.c_str(), "v %i %i %i", &result.x, &result.y, &result.z);
 
 	m_Faces.push_back(result);
-}
-
-void ObjReader::GetCommand(const std::string& line, const char stringSize)
-{
-	char result[255];
-	sscanf_s(line.c_str(), "# %s", result, stringSize);
-	result[stringSize] = 0;
-
-	m_Commands.push_back(result);
 }
--- Minigin/Structs.h
+++ Minigin/Structs.h
@@ -5,11 +5,6 @@ struct Vertex
 	float x, y, z;
 };
 
-struct VertexNormals
-{
-	float x, y, z;
-};
-
 struct Face
 {
 	int x, y, z;
